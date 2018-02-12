96d2cb49e81f878b10d484a0fd8969c6c36b2f7f
jogax <jozsef.romhanyi@gmail.com>
2018-02-12 14:45:19 +0100

    ruWordList second

---
+++ MagicOfWords/WordTrisClassen/WordTrisShape.swift
@@ -0,0 +1,22 @@
+//
+//  WordTrisShape.swift
+//  MagicOfWords
+//
+//  Created by Jozsef Romhanyi on 11/02/2018.
+//  Copyright © 2018 Jozsef Romhanyi. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+class WordTrisShape: SKShapeNode {
+    var type: MyShapes
+    init (type: MyShapes) {
+        self.type = type
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
